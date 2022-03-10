826b5d11cbba983b3534a67e1c539b626c8bd5e2
th3tasigma <46354465+th3tasigma@users.noreply.github.com>
2022-03-10 06:30:26 +0100

    openlayers+carte
    Test de carte openlayers pour les gens du sdis

---
+++ carte.js
@@ -0,0 +1,19 @@
+function init() {
+	var map = new OpenLayers.Map('地図',{controls:[]});
+	var osmLayer = new OpenLayers.Layer.OSM();
+	var gsat = new OpenLayers.Layer.Google('GoogleMaps',{type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22,
+sphericalMercator: true});
+	map.addLayer(osmLayer);
+	map.addLayer(gsat);
+	var mapProj = map.getProjectionObject();
+	var epsg4326 = new OpenLayers.Projection("EPSG:4326");
+	var center4326 = new OpenLayers.LonLat(139.480278,35.299722);
+	map.addControl(new OpenLayers.Control.Navigation());
+	var center = center4326.transform(epsg4326,mapProj);
+	var zoom = 16;
+	map.setCenter (center,zoom);
+	map.addControl(new OpenLayers.Control.PanZoomBar());
+	map.addControl(new OpenLayers.Control.MousePosition({displayProjection: epsg4326}));
+	map.addControl(new OpenLayers.Control.OverviewMap());
+	map.addControl(new OpenLayers.Control.LayerSwitcher());
+}
